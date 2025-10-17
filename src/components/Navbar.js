4f0c4f7f64c1bd79a50e65596cb6b64932a4e495
Julián España <julii1295@gmail.com>
2025-10-17 10:37:04 -0500

    Modificando estilos del navbar para mejor despliegue de enlaces

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -64,12 +64,12 @@ export default function Navbar() {
           <div className="lg:hidden flex items-center space-x-2">
             <button
               onClick={() => setIsMenuOpen(!isMenuOpen)}
-              className="relative p-2 bg-white/10 rounded-full hover:bg-gray-100 transition-colors duration-200 shadow-lg focus:outline-none"
+              className="relative p-1 bg-white rounded-full hover:bg-gray-100 transition-colors duration-200 shadow-lg focus:outline-none border-solid border-1 border-black"
             >
               <svg
                 // barras menu navbar
 
-                className="h-6 w-6 text-black [text-shadow:_2px_2px_0_#000,_-2px_-2px_0_#000,_2px_-2px_0_#000,_-2px_2px_0_#000] drop-shadow-[0_4px_3px_rgba(0,0,0,0.5)] rounded-lg "
+                className="h-6 w-6 text-black [text-shadow:_2px_2px_0_#000,_-2px_-2px_0_#000,_2px_-2px_0_#000,_-2px_2px_0_#000] drop-shadow-[0_4px_3px_rgba(0,0,0,0.5)] rounded-lg duration-500  transition-transform scale-3d"
                 fill="none"
                 strokeLinecap="round"
                 strokeLinejoin="round"
@@ -91,31 +91,29 @@ export default function Navbar() {
         {
           <div
             id="mobile-menu-backdrop"
-            className={`lg:hidden fixed inset-0 z-40 flex items-start justify-center pt-20`}
+            className={`lg:hidden fixed inset-0 z-40 flex items-start justify-center pt-20  ${styles.mobileMenuBackdrop}`}
             onClick={handleBackdropClick}
             style={{ pointerEvents: isMenuOpen ? "auto" : "none" }}
           >
             <div
               className={`
-                bg-[#148f54] rounded-xl shadow-lg w-11/12 max-w-xs mx-auto flex flex-col items-center py-6 space-y-3 z-50
-                transition-all duration-300
+                ${
+                  styles.mobileMenu
+                } rounded-4xl shadow-lg w-11/12 max-w-xs mx-auto flex flex-col items-center py-6 space-y-3 z-50
                 ${
                   isMenuOpen
                     ? "opacity-100 scale-100 translate-y-0 pointer-events-auto"
                     : "opacity-0 scale-95 -translate-y-10 pointer-events-none"
                 }
-                backdrop-blur-md
-              `}
-              style={{
-                transitionProperty: "opacity, transform",
-              }}
+               `}
               onClick={(e) => e.stopPropagation()}
             >
+              {/* Enlaces de navegación móvil */}
               {navigationLinks.map((link) => (
                 <Link
                   key={link.href}
                   href={link.href}
-                  className="text-white hover:text-green-200 px-4 py-2 rounded-md text-base font-medium transition-colors duration-200 text-center w-full"
+                  className={`text-black text-2xl  px-4 py-2 rounded-md  font-medium transition-colors duration-200 text-center w-full ${styles.mobileLink}`}
                   onClick={() => setIsMenuOpen(false)}
                 >
                   {link.label}
